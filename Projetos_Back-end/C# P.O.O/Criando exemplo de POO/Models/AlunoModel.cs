6ad4d6f6846aedcc37417e3dae9be7c69f81ce0b
LucSilveira <lucas1-portal@hotmail.com>
2019-09-27 11:42:16 -0300

    Subindo projetos referentes a P.O.O

---
+++ Projetos_Back-end/C# P.O.O/Criando exemplo de POO/Models/AlunoModel.cs
@@ -0,0 +1,13 @@
+namespace Criando_exemplo_de_POO.Models
+{
+    public class AlunoModel
+    {
+        string Nome {get; set; }
+
+        public string Curso { get; set; }
+
+        public int Rg { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
---
+++ Projetos_Back-end/C# P.O.O/Criando exemplo de POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Criando_exemplo_de_POO.Models;
+
+namespace Criando_exemplo_de_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlunoModel aluno1 = new AlunoModel();
+
+            Console.Clear();
+
+            System.Console.WriteLine("Insira o nome:");
+            aluno1.Nome = Console.ReadLine();
+
+            System.Console.WriteLine("Insira o nome do curso:");
+            aluno1.Curso = Console.ReadLine();
+
+            System.Console.WriteLine("Insira seu Rg:");
+            aluno1.Rg = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Informe sua idade:");
+            aluno1.Idade = int.Parse(Console.ReadLine());
+        }
+    }
+}
